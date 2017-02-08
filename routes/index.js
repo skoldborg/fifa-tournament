9ef1a2aa7b289466675053ee36469b9effbacee4
Pierre Sköldborg <pierre.skoldborg@creuna.se>
2017-02-08 10:53:00 +0100

    ..

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const Team = require('../db');
 const teams = require('./teams');
 const viewsDir = 'src/templates/views/';
--- routes/teams.js
+++ routes/teams.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const Teams = require('../db');
 
 const viewsDir = 'src/templates/views/';
