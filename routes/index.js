e252774ecc0ed218f8126b7ecdac13b69cd58a1c
Pierre Sköldborg <pierre.skoldborg@creuna.se>
2017-02-09 11:37:43 +0100

    I dont know

--- routes/index.js
+++ routes/index.js
@@ -3,83 +3,26 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const Team = require('../db');
+const Teams = require('../db');
 const teams = require('./teams');
 const viewsDir = 'src/templates/views/';
-const jadeTemplate = path.resolve(path.join(path.join(__dirname, '../'), viewsDir, 'index.jade'));
 
 router.get('/', teams.fetch);
 
 router.get('/result', (req, res) => {
-    const jadeTemplate = path.resolve(path.join(path.join(__dirname, '../'), viewsDir, 'result.jade'));
+    let jadeTemplate = path.resolve(path.join(path.join(__dirname, '../'), viewsDir, 'result.jade'));
+    
+    Teams.find().exec(function(err, teams) {
+        if(err) res.send(err);
 
-    res.render(jadeTemplate, { title: 'Register result' });
+        res.render(jadeTemplate, { title: 'Register result', teams: teams });
+    });
 });
 
-router.post('/update', (req, res) => {
-    let result = req.body;
-
-    // update home team
-    Team.findOne({ name: result.homeTeam }, function(err, team) {
-        if (err) throw err;
-
-        let updatedStats = {
-            gp: team.stats.gp + 1,
-            w: team.stats.w,
-            d: team.stats.d,
-            l: team.stats.l,
-            gf: team.stats.gf + parseInt(result.homeScore),
-            ga: team.stats.ga + parseInt(result.awayScore)
-        };
-
-        if (result.homeScore > result.awayScore) {
-            updatedStats['w'] = team.stats.w += 1;
-        } else if (result.homeScore === result.awayScore) {
-            updatedStats['d'] = team.stats.d += 1;
-        } else {
-            updatedStats['l'] = team.stats.l += 1;
-        }
-
-        team.stats = updatedStats;
-
-        team.save(function(err) {
-            if (err) throw err;
-        });
-    });
-
-    // update away team
-    Team.findOne({ name: result.awayTeam }, function(err, team) {
-        if (err) throw err;
-
-        let updatedStats = {
-            gp: team.stats.gp + 1,
-            w: team.stats.w,
-            d: team.stats.d,
-            l: team.stats.l,
-            gf: team.stats.gf + parseInt(result.awayScore),
-            ga: team.stats.ga + parseInt(result.homeScore)
-        };
-
-        if (result.homeScore > result.awayScore) {
-            updatedStats['l'] = team.stats.l += 1;
-        } else if (result.homeScore === result.awayScore) {
-            updatedStats['d'] = team.stats.d += 1;
-        } else {
-            updatedStats['w'] = team.stats.l += 1;
-        }
-
-        team.stats = updatedStats;
-
-        team.save(function(err) {
-            if (err) throw err;
-        });
-    });
-
-    res.redirect('/result');
-});
+router.post('/update', teams.update);
 
 router.get('/reset/:team', function(req, res) {
-    Team.findOne({ name: req.params.team }, function(err, team) {
+    Teams.findOne({ name: req.params.team }, function(err, team) {
         team.stats = {
             gp: 0,
             w: 0,
--- routes/teams.js
+++ routes/teams.js
@@ -11,5 +11,67 @@ module.exports = {
 
             res.render(jadeTemplate, { title: 'Creuna Fifa League', teams: teams });
         });
+    },
+
+    update: function(req, res) {
+        let result = req.body;
+
+        // update home team
+        Teams.findOne({ name: result.homeTeam }, function(err, team) {
+            if (err) throw err;
+
+            let updatedStats = {
+                gp: team.stats.gp + 1,
+                w: team.stats.w,
+                d: team.stats.d,
+                l: team.stats.l,
+                gf: team.stats.gf + parseInt(result.homeScore),
+                ga: team.stats.ga + parseInt(result.awayScore)
+            };
+
+            if (result.homeScore > result.awayScore) {
+                updatedStats['w'] = team.stats.w += 1;
+            } else if (result.homeScore === result.awayScore) {
+                updatedStats['d'] = team.stats.d += 1;
+            } else {
+                updatedStats['l'] = team.stats.l += 1;
+            }
+
+            team.stats = updatedStats;
+
+            team.save(function(err) {
+                if (err) throw err;
+            });
+        });
+
+        // update away team
+        Teams.findOne({ name: result.awayTeam }, function(err, team) {
+            if (err) throw err;
+
+            let updatedStats = {
+                gp: team.stats.gp + 1,
+                w: team.stats.w,
+                d: team.stats.d,
+                l: team.stats.l,
+                gf: team.stats.gf + parseInt(result.awayScore),
+                ga: team.stats.ga + parseInt(result.homeScore)
+            };
+
+            if (result.homeScore > result.awayScore) {
+                updatedStats['l'] = team.stats.l += 1;
+            } else if (result.homeScore === result.awayScore) {
+                updatedStats['d'] = team.stats.d += 1;
+            } else {
+                updatedStats['w'] = team.stats.l += 1;
+            }
+
+            team.stats = updatedStats;
+
+            team.save(function(err) {
+                if (err) throw err;
+            });
+        });
+
+        res.redirect('/result');
     }
 }
